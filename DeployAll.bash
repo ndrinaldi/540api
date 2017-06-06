3fffd7676b0c28a51124d241d1a44556767255f6
ndrinaldi <Nicholas@Loanys-MacBook-Pro.local>
2017-06-06 14:44:03 -0400

    refactorde the cloudformation

---
+++ DeployAll.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd /Users/nicholasrinaldi/www-540-api/Clients
+serverless deploy
+
+cd /Users/nicholasrinaldi/www-540-api/ContractVehicles
+serverless deploy
+
+cd /Users/nicholasrinaldi/www-540-api/Partners
+serverless deploy
+
+cd /Users/nicholasrinaldi/www-540-api/Projects
+serverless deploy
+
+cd /Users/nicholasrinaldi/www-540-api/Services
+serverless deploy
+
+cd /Users/nicholasrinaldi/www-540-api/TeamMembers
+serverless deploy
